99e53205776ed00c638935a8aed53d318fbf4009
matsufuji06 <n.matsufuji06@gmail.com>
2021-09-12 14:46:38 +0900

    JSの配列で一枚目のヘッド画像を表示

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+  const images = [
+    "img/head01.jpg",
+    "img/head02.jpg"
+  ];
+
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById("head_img");
+  mainImage.src = images[currentIndex];
+}
