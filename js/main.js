3ba6b8850ee1e7083cfc409b6be30a525733fad7
matsufuji06 <n.matsufuji06@gmail.com>
2021-09-12 16:14:59 +0900

    進めると戻すのスライド実装（※不具合あり）

--- js/main.js
+++ js/main.js
@@ -3,11 +3,33 @@
 {
   const images = [
     "img/head01.jpg",
-    "img/head02.jpg"
+    "img/head02.jpg",
+    "img/head03.jpg"
   ];
 
   let currentIndex = 0;
 
-  const mainImage = document.getElementById("head_img");
+  let mainImage = document.getElementById("head_img");
   mainImage.src = images[currentIndex];
+  
+  const next = document.getElementById("next");
+  next.addEventListener("click", () => {
+    let target = currentIndex + 1;
+    if (target === 3) {
+      mainImage.src = images[0];
+    } else {
+      mainImage.src = images[target];
+    }
+  });
+  
+  const prev = document.getElementById("prev");
+  prev.addEventListener("click", () => {
+    let target = currentIndex - 1;
+    if (target < 0) {
+      mainImage.src = images[2];
+    } else {
+      mainImage.src = images[target];
+    }
+
+  });
 }
